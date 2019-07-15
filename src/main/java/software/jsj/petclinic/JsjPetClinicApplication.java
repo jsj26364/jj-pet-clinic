583160469abb4cc6b878d587c2159eeb41d7765b
jsj26364 <jsj26364@gmail.com>
2019-07-15 13:37:09 +1000

    Initial Commit

---
+++ src/main/java/software/jsj/petclinic/JsjPetClinicApplication.java
@@ -0,0 +1,13 @@
+package software.jsj.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsjPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsjPetClinicApplication.class, args);
+	}
+
+}
