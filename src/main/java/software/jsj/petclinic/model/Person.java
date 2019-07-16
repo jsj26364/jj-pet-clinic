c1748fad0221d6884f8ce4a08020e0968c4cfec5
jsj26364 <jsj26364@gmail.com>
2019-07-17 00:11:00 +1000

    closes #2

--- src/main/java/software/jsj/petclinic/model/Person.java
+++ src/main/java/software/jsj/petclinic/model/Person.java
@@ -15,12 +15,15 @@ public class Person {
   public String getFirstName() {
     return firstName;
   }
+  
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
+  
   public String getLastName() {
     return lastName;
   }
+  
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
