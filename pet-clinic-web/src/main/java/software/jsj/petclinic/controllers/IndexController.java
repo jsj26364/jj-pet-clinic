8d079f71de5baa125ceada59666f69305d0a62b5
jsj26364 <jsj26364@gmail.com>
2019-07-18 21:54:41 +1000

    Add Index page & Controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/software/jsj/petclinic/controllers/IndexController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package software.jsj.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author jsanyang
+ *
+ */
+@Controller
+public class IndexController {
+
+  @RequestMapping({"/", "", "index", "index.html"})
+  public String index() {
+    
+    return "index";
+  }
+  
+}
