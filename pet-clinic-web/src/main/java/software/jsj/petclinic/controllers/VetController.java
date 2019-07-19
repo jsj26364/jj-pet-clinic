be908b2e77ffdb81924e5d3e1348636f571382eb
jsj26364 <jsj26364@gmail.com>
2019-07-19 17:41:31 +1000

    Add and display Vets list and update associated controller. Closes #15

--- pet-clinic-web/src/main/java/software/jsj/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/software/jsj/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package software.jsj.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import software.jsj.petclinic.services.VetService;
 
 @Controller
 public class VetController {
   
+  private final VetService vetService;
+  
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
+
   @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-  public String listVets() {
+  public String listVets(Model model) {
+    
+    model.addAttribute("vets", vetService.findAll());
     
     return "vets/index";
   }
