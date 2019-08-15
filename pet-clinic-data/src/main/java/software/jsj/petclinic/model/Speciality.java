7fe5f2e627227df56efbd82b46bdc54334082faa
jsj26364 <jsj26364@gmail.com>
2019-08-16 00:49:33 +1000

    Add Speciality entiry, associate it with Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/software/jsj/petclinic/model/Speciality.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package software.jsj.petclinic.model;
+
+/**
+ * @author jsjackson
+ *
+ */
+public class Speciality extends BaseEntity {
+
+  private String description;
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  
+}
