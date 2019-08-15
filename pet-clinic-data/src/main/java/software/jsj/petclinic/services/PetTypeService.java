b505cfffb5e64b0ddfaa36237dea1c849e8bf2fd
jsj26364 <jsj26364@gmail.com>
2019-08-16 01:19:33 +1000

    Add PetTypeService & its map implementation. Closes #29

---
+++ pet-clinic-data/src/main/java/software/jsj/petclinic/services/PetTypeService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package software.jsj.petclinic.services;
+
+import software.jsj.petclinic.model.PetType;
+
+/**
+ * @author jsjackson
+ *
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
