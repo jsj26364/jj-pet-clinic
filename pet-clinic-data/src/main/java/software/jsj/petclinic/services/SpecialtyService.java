01e1ba6d1aca3cfbe04ad4819a687a70a6d13a48
jsj26364 <jsj26364@gmail.com>
2019-08-16 22:19:06 +1000

    Add Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/software/jsj/petclinic/services/SpecialtyService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package software.jsj.petclinic.services;
+
+import software.jsj.petclinic.model.Specialty;
+
+/**
+ * @author jsjackson
+ *
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/software/jsj/petclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package software.jsj.petclinic.services.map;
+
+import java.util.Set;
+import org.springframework.stereotype.Service;
+import software.jsj.petclinic.model.Specialty;
+import software.jsj.petclinic.services.SpecialtyService;
+
+/**
+ * @author jsjackson
+ *
+ */
+@Service
+public class SpecialtyMapService  extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+  @Override
+  public Set<Specialty> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Specialty findById(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Specialty save(Specialty object) {
+    return super.save(object);
+  }
+
+  @Override
+  public void delete(Specialty object) {
+    super.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+
+}
